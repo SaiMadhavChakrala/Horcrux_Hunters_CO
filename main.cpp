ad141a8a54a8c73ef84cf78006d28d1df8947297
Jyothiraditya <jyothiraditya1278@gmail.com>
2024-03-10 15:43:53 +0530

    main.cpp update

--- main.cpp
+++ main.cpp
@@ -6,12 +6,10 @@ std::string ltrim(const std::string &s)
 {
     return std::regex_replace(s, std::regex("^\\s+"), std::string(""));
 }
-
 std::string rtrim(const std::string &s)
 {
     return std::regex_replace(s, std::regex("\\s+$"), std::string(""));
 }
-
 std::string trim(const std::string &s)
 {
     return ltrim(rtrim(s));
